aa02af23f132f5228a8282da5d6914a06f1d5f7b
cravinx <unegbuprecious25@gmail.com>
2024-10-19 13:02:31 +0200

    first commit

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container flexSB'>
+          <div className='logo'>
+            <h1>ACADEMIA</h1>
+            <span>ONLINE EDUCATION & LEARNING</span>
+          </div>
+
+          <div className='social'>
+            <i className='fa fa-facebook-f icon'></i>
+            <i className='fa fa-instagram icon'></i>
+            <i className='fa fa-twitter icon'></i>
+            <i className='fa fa-youtube icon'></i>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
